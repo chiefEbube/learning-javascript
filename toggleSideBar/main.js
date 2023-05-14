d569610b4072f8fbf13c41759fc646d08f658a79
ebube-anyanwu <ebubechianyanwu1@gmail.com>
2023-05-14 21:44:31 +0100

    Last changes

--- toggleSideBar/main.js
+++ toggleSideBar/main.js
@@ -1,12 +1,13 @@
 let toggleStatus = false;
 
-let toggleSideBar = function() {
+
     let getSidebar = document.querySelector(".side-bar");
     let getSidebarUl = document.querySelector(".side-bar ul");
     let getSidebarA = document.querySelectorAll(".side-bar a");
 
 
-    if (!toggleStatus){
+    // if (!toggleStatus)
+    let toggleSideBar = function() {
         getSidebarUl.style.visibility = "visible";
         getSidebar.style.width = "200px"
 
@@ -20,7 +21,7 @@ let toggleSideBar = function() {
         toggleStatus = true;
     }
 
-    else if (toggleStatus){
+    let leave = () => {
         getSidebar.style.width = "50px"
 
         let arrLength = getSidebarA.length;
@@ -33,4 +34,3 @@ let toggleSideBar = function() {
         getSidebarUl.style.visibility = "hidden"
         toggleStatus = false;
     }
-}
