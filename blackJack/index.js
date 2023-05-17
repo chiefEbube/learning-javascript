b09feed602687b0b0d29fbd065e4de0a3f39d987
ebube-anyanwu <ebubechianyanwu1@gmail.com>
2023-05-17 18:35:10 +0100

    commit

---
+++ blackJack/index.js
@@ -0,0 +1,6 @@
+function rollDice(){
+     return Math.floor(Math.random() * 6) + 1
+}
+
+console.log(rollDice())
+
--- blackJack/main.js
+++ blackJack/main.js
@@ -1,32 +1,56 @@
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 const messageElement = document.getElementById("message")
 
 const sumElement = document.getElementById("sum")
 const cardsElement = document.querySelector("#cards")
 
-const startGame = () => {
-    cardsElement.textContent = "Cards: " + firstCard + " " + secondCard
+function getRandomCard() {
+    let randomNum = Math.ceil(Math.random() * 13)
+    if (randomNum === 1) { return 11 }
+    else if (randomNum > 10) { return 10 }
+    else { return randomNum }
+}
+
+const renderGame = () => {
+    cardsElement.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsElement.textContent += cards[i] + " "
+    }
     sumElement.textContent = "Sum: " + sum;
 
     if (sum <= 20) {
-        message = "Do you want bla bla bla"
+        message = "Do you want to draw a new card?"
     }
     else if (sum === 21) {
         message = "BlackJack"
         hasBlackJack = true
     }
     else {
-        message = "You're out bro"
+        message = "You're out, bro"
         isAlive = false
     }
     messageElement.textContent = message
 }
 
+const startGame = () => {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = cards[0] + cards[1]
+    renderGame()
+}
+
 const newCard = () => {
-    console.log("Drawing a new card")
+    if (isAlive && !hasBlackJack) {
+        let card = getRandomCard()
+        sum += card;
+        cards.push(card)
+        renderGame()
+    }
+
 }
