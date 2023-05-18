193ef4c8da97e7e9fe4a401a8296a7dafb0bd560
ebube-anyanwu <ebubechianyanwu1@gmail.com>
2023-05-18 09:58:55 +0100

    added randomFruits

--- blackJack/index.js
+++ blackJack/index.js
@@ -1,9 +1,29 @@
-let castle = {
-     name: "Dairsie Castle",
-     guests: 14,
-     isFree: false,
-     content: ["beds", "baths"]
+let hands = ["rock", "paper", "scissors"]
+
+function randHands() {
+     let i = Math.floor(Math.random() * 3)
+     return hands[i]
 }
 
-console.log(castle.name)
-console.log(castle.content)
+console.log(randHands())
+
+
+
+// let dayOfMonth = 13
+// let weekday = "Friday"
+// if (weekday === "Friday" && dayOfMonth === 13){
+//      console.log("spooky face")
+// }
+
+
+// let largeCountries = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"]
+// largeCountries.pop()
+// largeCountries.push("Parkistan")
+// largeCountries.shift()
+// largeCountries.unshift("China")
+
+// console.log("The 5 largest countries in the world:")
+
+// for (let i = 0; i < largeCountries.length; i++){
+//      console.log("- " + largeCountries[i])
+// }
